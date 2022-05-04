ea297533339872c9be8cb7486d95658ce5dd80b6
Tricky360 <58693157+Tricky360@users.noreply.github.com>
2022-05-04 15:18:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,20 +69,7 @@ def allmsg(Client, message):
     else:
         print('can not understand')
 
-@server.route('/' + BOT_TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://best-shanu-m-bot.herokuapp.com/' + BOT_TOKEN)
-    return "!", 200
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.run(debug=True)
