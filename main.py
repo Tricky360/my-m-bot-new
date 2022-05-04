ad81c99b0bd06d0cd22a63b350cce7b47a36d452
Tricky360 <58693157+Tricky360@users.noreply.github.com>
2022-05-04 15:06:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ bot = Client(
 # server = Flask(__name__)
 # #add
 
+
 @bot.on_message(filters.command('api'))
 def add_api(c, m):
     print(m)
@@ -40,6 +41,7 @@ def add_api(c, m):
     else:
         m.reply_text('16 character API is valid, please try again!!!')
 
+
 @bot.on_message()
 def allmsg(Client, message):
     user_id = str(message.chat.id)
@@ -47,41 +49,40 @@ def allmsg(Client, message):
         print('Text Message')
         print(message)
         Db.my_data(message)
-        txt= message.text
-        ent= caption_ent(message.entities)
+        txt = message.text
+        ent = caption_ent(message.entities)
 
-        bot.send_message(760506822, replace_caption(txt, user_id), entities=ent)
+        bot.send_message(760506822, replace_caption(
+            txt, user_id), entities=ent)
 
     elif (message.text and message.reply_markup):  # Text + Reply Markup
         print('Text with Reply Markup Message')
 
     elif (message.caption and not message.reply_markup):  # caption
         print('Caption Message')
-        txt= message.text
+        txt = message.text
 
     elif (message.caption and message.reply_markup):  # caption + Reply Markup
         print('Caption with Reply Markup Message')
-        txt= message.text
+        txt = message.text
 
     else:
         print('can not understand')
 
-# @server.route('/' + TOKEN, methods=['POST'])
-# def getMessage():
-#     json_string = request.get_data().decode('utf-8')
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])
-#     return "!", 200
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 
-# @server.route("/")
-# def webhook():
-#     bot.remove_webhook()
-#     bot.set_webhook(url='https://best-shanu-m-bot.herokuapp.com/' + TOKEN)
-#     return "!", 200
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://best-shanu-m-bot.herokuapp.com/' + BOT_TOKEN)
+    return "!", 200
 
 
-# if __name__ == "__main__":
-#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    
-bot.run()
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
