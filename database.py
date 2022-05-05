46c2433e146d0746ebc4e05f5510f05a3d5df667
Tricky360 <58693157+Tricky360@users.noreply.github.com>
2022-05-05 10:10:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ class Db:
 
     @classmethod
     def my_data(cls, msg):
-        user_id = str(msg['chat']['id'])
+#         user_id = str(msg['chat']['id'])
         user_name = msg['chat']['first_name']
         join_date = msg['date']
 
